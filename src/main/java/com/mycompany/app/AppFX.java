5edaf61fca8878825a9d1f0d76ff84776bdbd724
landinij0040 <ijlandin004@gmail.com>
2022-05-06 21:46:48 -0500

    Added Payment Prompts

--- src/main/java/com/mycompany/app/AppFX.java
+++ src/main/java/com/mycompany/app/AppFX.java
@@ -30,8 +30,10 @@ import java.nio.file.FileAlreadyExistsException;
 // Used to Read the JSON file
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.NumberFormatException;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -40,7 +42,28 @@ import org.json.simple.parser.ParseException;
 import java.util.*;
 
 public class AppFX extends Application {
+    static ArrayList<ObservableList<String>> allItems = new ArrayList<ObservableList<String>>();
+    static Label loading = new Label("Enter new Item");
+    // For making a prompt
+    static final HBox hb = new HBox();
+    static final TextField itemRow = new TextField();
+    static final TextField itemColumn = new TextField();
+    static final Button btn = new Button("Select");
+    // For making payment
+    static final HBox hb2 = new HBox();
+    static Label paymentLabel = new Label();
+    static final TextField payment = new TextField();
+    static final Button btn2 = new Button("Select");
+
+    static VBox vbox = new VBox();
     
+    
+    // For making a new item
+    static final TextField itemName = new TextField();
+    static final TextField itemPrice = new TextField();
+    static final TextField itemAmount = new TextField();
+    static JSONObject json = readJSONConfig("src\\main\\java\\com\\mycompany\\app\\input.json");
+
     @Override
     public void start(Stage stage) {
         // Setting up the label for the vending machine
@@ -49,14 +72,11 @@ public class AppFX extends Application {
         // Setting up the prompt label
         Label prompt = new Label("Welcome Enter Your Selection");
         prompt.setFont(new Font("Arial",20));
-        
-
-        
-        
+    
         final HBox promptHBox = new HBox();
         promptHBox.getChildren().addAll(prompt);
         // Getting the JSON
-        JSONObject json = readJSONConfig("src\\main\\java\\com\\mycompany\\app\\input.json");
+        
         JSONObject config = (JSONObject)json.get("config");
         JSONArray items = (JSONArray)json.get("items");
         int itemsAmount = items.size();
@@ -77,7 +97,7 @@ public class AppFX extends Application {
             final int finalIdx = i;
             TableColumn <ObservableList<String>,String> tc = new TableColumn<>(Integer.toString(i + 1));
             tc.setCellValueFactory(param ->{ 
-                System.out.println(param.getValue().get(0)); 
+                // System.out.println(param.getValue().get(0)); 
                 return new ReadOnlyObjectWrapper(param.getValue().get(finalIdx + 1)); 
             });
             table.getColumns().add(tc);
@@ -93,76 +113,80 @@ public class AppFX extends Application {
             // Going througth all the available slots 
             for(int aval = 0; aval < columns; aval++){
                 String itemToPut = "";
-                System.out.println(currentItem);
-                System.out.println(currentItem != itemsAmount);
                 if(currentItem < itemsAmount){
                     JSONObject item = (JSONObject)items.get(currentItem);
                     itemToPut = (String)item.get("name") + "\n";
                     itemToPut = itemToPut + "amount: " +  String.valueOf(item.get("amount")) + "\n";
-                    itemToPut = itemToPut + "price: " +   (String)item.get("price");
-                    System.out.println(itemToPut); //TODO: Delete           
+                    itemToPut = itemToPut + "price: " +   (String)item.get("price");          
                     currentItem++; 
                 }
                 currentRow.add(itemToPut);
             }
+            // Adding to all items list
+            allItems.add(currentRow);
             table.getItems().add(currentRow);
 
         }    
-        // Setting up the selction options
-        final HBox hb = new HBox();
-        final TextField itemRow = new TextField();
+        // Setting up the prompt
         itemRow.setPromptText("Row");
-        final TextField itemColumn = new TextField();
         itemColumn.setPromptText("Column");
-        final TextField payment = new TextField();
         payment.setPromptText("Payment");
-
-        final Button btn = new Button("Add");
         btn.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent e){
-                System.out.println(itemRow.getText());
-                System.out.println(itemColumn.getText());
-                System.out.println(payment.getText());
+                // System.out.println(itemRow.getText());
+                // System.out.println(itemColumn.getText());
+                // System.out.println(payment.getText());
                 // prompt.setOnInputMethodTextChanged(arg0);
                 // prompt.applyCss("color: red;");
-
-                System.out.println();
-                prompt.setText("Switch");
+                payment.setOpacity(100);
+                btn2.setOpacity(100);
+                paymentLabel.setOpacity(100);
                 itemRow.clear();
                 itemColumn.clear();
-                payment.clear();
             }
         });
         hb.getChildren().addAll(itemRow, itemColumn, payment,btn);
 
+        // Setting up the payment
+        paymentLabel.setText("Please enter payment");
+        paymentLabel.setOpacity(0);
+        payment.setPromptText("Payment");
+        payment.setOpacity(0);
+        btn2.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){
+                payment.setOpacity(0);
+                btn2.setOpacity(0);
+                paymentLabel.setOpacity(0);
+            } 
+        });
+        btn2.setOpacity(0);
+        hb2.getChildren().addAll(payment,btn2);
     
         // Setting up the loading options
         final HBox loadingHBoxLabel = new HBox();
-        Label loading = new Label("Enter new Item");
+        
         loading.setFont(new Font("Arial",20));
         loadingHBoxLabel.getChildren().addAll(loading);
         final HBox loadingHBox = new HBox();
-        final TextField itemName = new TextField();
         itemName.setPromptText("New Item Name");
-        final TextField itemAmount = new TextField();
         itemAmount.setPromptText("New Item Amount");
-        final TextField itemPrice = new TextField();
         itemPrice.setPromptText("New Price");
-        loadingHBox.getChildren().addAll(itemName, itemAmount, itemPrice);
-        
-        
-    
-
-        
-        
-        // Setting up the button
+        Button add = new Button("Add Item");
+        add.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){
+                addItem();
+                table.refresh();
+            } 
+        });
+        loadingHBox.getChildren().addAll(itemName, itemAmount, itemPrice, add);
         
         // Setting up the Vbox
-        VBox vbox = new VBox();
         // vbox.setSpacing(5);
         // vbox.setPadding(new Insets(10,0,0,10));
-        vbox.getChildren().addAll(label, table, promptHBox,hb, loadingHBoxLabel,loadingHBox);
+        vbox.getChildren().addAll(label, table, promptHBox,hb,paymentLabel,hb2, loadingHBoxLabel,loadingHBox);
         Scene scene = new Scene(new Group());
         // TODO make the style sheet work lol
         // Adding the styling sheet
@@ -183,12 +207,48 @@ public class AppFX extends Application {
         launch();
     }
 
-    public static addItem(){
+    public static void addItem(){
         // Update the ArrayList
-        // Update the Json
+        // Find the next available slot
+        for(int i = 0; i < allItems.size(); i++){
+            ObservableList<String> currentRow = allItems.get(i);
+            System.out.println(currentRow);
+            for(int j = 0; j < currentRow.size(); j++){
+                if (currentRow.get(j).equals("")){
+                    ObservableList<String> newRow = FXCollections.observableArrayList(currentRow);
+                    // Making the new item
+                    String newItemName = itemName.getText();
+                    String newItemPrice = itemPrice.getText();
+                    String newItemAmount = itemAmount.getText();
+                    if( !newItemName.equals("") && !newItemPrice.equals("") && !newItemAmount.equals("") ){
+                        String newItem = newItemName + "\n" +  "amount: " + newItemAmount +  "\n" + "price: $" + newItemPrice;
+                        System.out.println(newItem); // TODO: Delete;
+                        newRow.set(j, newItem);
+                        currentRow.removeAll(currentRow);
+                        currentRow.addAll(newRow);
+                        itemName.clear();
+                        itemPrice.clear();
+                        itemAmount.clear();
+                        // Up date the json
+                        JSONObject newJSONObject = new JSONObject();
+                        newJSONObject.put("amount", Long.parseLong(newItemAmount));
+                        newJSONObject.put("price", newItemPrice);
+                        newJSONObject.put("name",newItemName);
+                        
+                        JSONArray itemsList = (JSONArray)json.get("items");
+                        itemsList.add(newJSONObject);
+                        writeJSONconfig();
+                    }
+                    return;
+                }
+            }
+        }
+        loading.setText("Sorry No More Room");
+        loading.setStyle("-fx-text-fill: red");
+        
     }
 
-    public static JSONObject readJSONConfig(String jsonPath){ 
+    public static JSONObject readJSONConfig(String jsonPath) throws NumberFormatException{ 
         // getting the json path
         String path = System.getProperty("user.dir") + "\\"+ jsonPath;
         // JSON parser object to parse read file
@@ -208,6 +268,16 @@ public class AppFX extends Application {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void writeJSONconfig(){
+        try(FileWriter file = new FileWriter("src/main/java/com/mycompany/app/input.json")){
+            file.write(json.toJSONString());
+            file.flush();
+            System.out.println("writeJSONConfig");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
 
